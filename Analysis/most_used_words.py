a9d50badc56637b5badcee1c7c017698e4400bee
Nikita <nikitasingla011@gmail.com>
2020-10-18 14:34:54 +0530

    analysis part updated

--- Analysis/most_used_words.py
+++ Analysis/most_used_words.py
@@ -6,14 +6,18 @@ import plotly.graph_objs as go
 import plotly
 
 
-def create_bar_graph():
+def audio_analysis():
 	text_dict={}
+	ans=[]
+	count=0
+	time= 2    #in minutes
 
 	stop_list=list(stopwords.words("english"))
 
 	with open('textfile.txt','r',encoding='utf-8') as f:
 		list1=f.read().lower().split()
 
+	
 	for i in range(0,len(list1)):
 		list1[i]=list1[i].strip('.,;:?!\\|/$#+*()')
 
@@ -23,25 +27,30 @@ def create_bar_graph():
 				text_dict[word]=text_dict[word]+1
 			else:
 				text_dict[word]=1
+		count=count+1
+
+	speaking_rate=count/time
+	ans.append(speaking_rate)
 
 	sort_list=sorted(text_dict.items(),key=op.itemgetter(1),reverse=True)
 	max_words=sort_list[:10]
+
+	ans.append(sort_list)
 
 	words=list(zip(*max_words))[0]
 	frequency=list(zip(*max_words))[1]
 
 	y_position=np.arange(len(words))
 
-
 	data = [go.Bar(x = words,y = frequency)]
 	fig = go.Figure(data=data, layout={'title':{'text': "FREQUENCIES OF THE MOST USED WORDS"}})
 	fig.show()
-	# plotly.io.write_html(fig,"h.html")
-	# return
+
 	html_string=plotly.io.to_html(fig)
-	return html_string
 
+	ans.append(html_string)
+	#print(ans[0])
+	#print(ans[1])
+	return ans
 
-
-# create_bar_graph()
-# print(x)
+#audio_analysis()
