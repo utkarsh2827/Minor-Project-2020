25a5b6b11383ae480ba0b8da684f6b9b153d02d7
Nikita <nikitasingla011@gmail.com>
2020-11-07 16:26:45 +0530

    stored text in results

--- InterviewSimplified/VideoAnalysis/audio_to_text.py
+++ InterviewSimplified/VideoAnalysis/audio_to_text.py
@@ -1,15 +1,13 @@
 import speech_recognition as sr
-
 def generate_text(audio_path):
     r = sr.Recognizer()
     # Reading Audio file as source
     # listening the audio file and store in audio_text variable
     file1=open("textfile.txt","w",encoding="utf-8")
-
+    text=''
     with sr.AudioFile(audio_path) as source: 
         audio_text = r.listen(source)
-
-    text=''    
+        
     # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
         try:
             # using google speech recognition
